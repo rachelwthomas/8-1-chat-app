0f4d52947135d7346aa45a84b718958d8ff14fbd
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-02-24 14:12:48 -0500

    sign up view working

--- accounts/views.py
+++ accounts/views.py
@@ -4,5 +4,5 @@ from django.urls import reverse_lazy
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lasy('login')
+    success_url = reverse_lazy('login')
     template_name = 'signup.html'
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views import generic
+
+
+class IndexView(TemplateView):
+    template_name = 'chat_app/index.html'
 
 # Create your views here.
